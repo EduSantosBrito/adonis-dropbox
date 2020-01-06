5beac4ea69201074a53966de3cd469fed9cbac5c
Brito <eduardo@viewup.com>
2020-01-06 13:08:32 -0300

    0.2.0-5

--- providers/AdonisDropboxProvider.js
+++ providers/AdonisDropboxProvider.js
@@ -5,7 +5,7 @@ class AdonisMongodbProvider extends ServiceProvider {
         this.app.singleton('Dropbox', () => {
             const Config = this.app.use('Adonis/Src/Config');
             const fetch = this.app.use('isomorphic-fetch');
-            const fileType = this.app.use('fileType');
+            const fileType = this.app.use('file-type');
             /** @type {import('dropbox/lib')} */
             const { Dropbox } = this.app.use('dropbox');
             return new (require('../src/AdonisDropbox'))({ Config, Dropbox, fetch, fileType });
